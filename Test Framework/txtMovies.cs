9416750edcf9b15e12712fd7a6080abe15143738
p16227391 <p16227391@my365.dmu.ac.uk>
2019-02-26 22:54:57 +0000

    created the clsMovies and the test class

---
+++ Test Framework/txtMovies.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Class_Library;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test_Framework
+{
+    [TestClass]
+    public class txtMovies
+    {
+        [TestMethod]
+        public void InstanceOk()
+        {
+            //create an instance of the class clsMovies
+            clsMovies AnMovies = new clsMovies();
+            //check to see that the class is not null
+            Assert.IsNotNull(AnMovies);
+        }
+    }
+}
