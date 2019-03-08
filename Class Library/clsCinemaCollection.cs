da6d237553e6583eafaacf5d3b7b1839691add2a
kinst <kinst@LAPTOP-6OALEC2B>
2019-03-08 23:59:52 +0000

    created the app data

--- Class Library/clsCinemaCollection.cs
+++ Class Library/clsCinemaCollection.cs
@@ -4,7 +4,36 @@ namespace Class_Library
 {
     public class clsCinemaCollection
     {
-        public int Count { get; set; }
-        public List<clsCinema> AllCinema { get; set; }
+        private List<clsCinema> mAllCinema = new List<clsCinema>();
+
+        //public property for count
+        public int Count
+        {
+            get
+            {
+                //return the count property of the private list
+                return mAllCinema.Count;
+            }
+            set
+            {
+
+            }
+        }
+        //public property for allmovies
+        public List<clsCinema> AllCinema
+        {
+            //getter sends data to requesting code
+            get
+            {
+                //return private data member
+                return mAllCinema;
+            }
+            //setter accepts data from other objects
+            set
+            {
+                //assign the incoming value to the private data member
+                mAllCinema = value;
+            }
+        }
     }
 }
